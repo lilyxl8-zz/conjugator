aa58b00085b9de6527c19ab46fd56d0230b8e4c9
Lily Zhang <ellabailasola@gmail.com>
2018-07-31 23:15:36 +0200

    v2.0: add german, study charts
    update docs
    
    add lang option
    
    refactor verbs logic
    
    chkpt
    
    consolidate él / ella
    
    add routes
    
    add conjugation charts
    
    styled 💅
    
    fix
    
    fix
    
    fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,122 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as Verbs from './verbs';
-const { spanishVerbs, spanishPronouns } = Verbs;
+const {
+  spanishVerbs, spanishPronouns, germanVerbs, germanPronouns,
+} = Verbs;
 
-const NUM_TOTAL = Object.keys(spanishVerbs).length || 10;
+const NUM_TOTAL = 10;
+
+const StartScreen = ({ chooseLanguage, wrongAnswerMsg }) => (
+  <div>
+    {wrongAnswerMsg && (
+      <div className="error">
+        {wrongAnswerMsg}
+      </div>
+    )}
+    <br />
+    {"Choose your language:"}
+    <br />
+    <br />
+    <br />
+    <div onClick={chooseLanguage("spanish")} className="button">
+      {"Spanish"}
+    </div>
+    <br />
+    <br />
+    <br />
+    <div onClick={chooseLanguage("german")} className="button">
+      {"German"}
+    </div>
+  </div>
+)
+
+class StudyScreen extends Component {
+  render() {
+    const { language, pronouns, conjugations, onNext } = this.props;
+    return (
+      <div>
+        <br />
+        <h1 style={{ textTransform: "capitalize"}}>{language}</h1>
+        <br />
+        <br />
+        <div onClick={onNext} className="button">
+          {"Start"}
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        {"These are the verbs you'll be training:"}
+        <br />
+        <br />
+        {
+          Object.keys(conjugations).map(verb => (
+            <div key={verb} className="verbContainer">
+              <h3>{`${verb.toUpperCase()} (${conjugations[verb].meaning})`}</h3>
+              {
+                pronouns.map(pronoun => (
+                  <div key={pronoun} className="conjugationContainer">
+                    <div><i>{pronoun}</i></div>
+                    <div>{conjugations[verb][pronoun]}</div>
+                  </div>
+                ))
+              }
+            </div>
+          ))
+        }
+        <br />
+        <br />
+        <br />
+        <div onClick={onNext} className="button">
+          {"Start"}
+        </div>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      numCorrect: 0,
-      verbList: [],
+      currentLanguage: '',
       pronoun: '',
+      currentVerb: '',
+      numAnswered: 0,
+      numCorrect: 0,
       input: '',
-      isStarted: false,
       wrongAnswerMsg: '',
+      route: 'home',
     };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.randomPronoun = this.randomPronoun.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+    this.startGame = this.startGame.bind(this);
   }
 
-  startGame = (e) => {
-    this.setState({
-      pronoun: this.randomPronoun(),
-      verbList: Object.keys(spanishVerbs),
-      isStarted: true,
-      numCorrect: 0,
-      wrongAnswerMsg: ''
-    });
+  conjugations(language) {
+    return language === 'spanish' ? spanishVerbs : germanVerbs;
+  }
+
+  verbs(language) {
+    return Object.keys(this.conjugations(language));
+  }
+
+  pronouns(language) {
+    return language === 'spanish' ? spanishPronouns : germanPronouns;
+  }
+
+  randomPronoun() {
+    const pronouns = this.pronouns(this.state.currentLanguage);
+    return this.sample(pronouns);
+  }
+
+  handleChange = (e) => {
+    this.setState({ input: e.target.value });
   }
 
   handleKeyPress = (e) => {
@@ -38,70 +125,99 @@ class App extends Component {
     }
   }
 
-  randomPronoun = () => spanishPronouns[Math.floor(Math.random() * spanishPronouns.length)];
-
-  handleChange = (e) => {
-    this.setState({ input: e.target.value });
+  sample(array) {
+    return array[Math.floor(Math.random() * array.length)]
   }
 
-  handleSubmit = (e) => {
-    const userAnswer = this.state.input;
-    const currentVerb = this.state.verbList[0]
-    const correctAnswer = spanishVerbs[currentVerb][this.state.pronoun];
+  handleNext = language => (e) => {
+    this.setState({ currentLanguage: language, route: 'study' });
+  }
 
-    let { verbList, numCorrect } = this.state;
-    verbList.shift();
-    const isGameOver = verbList.length === 0
+  startGame = (e) => {
+    const { currentLanguage } = this.state;
+    this.setState({
+      pronoun: this.sample(this.pronouns(currentLanguage)),
+      currentVerb: this.sample(this.verbs(currentLanguage)),
+      numAnswered: 0,
+      numCorrect: 0,
+      wrongAnswerMsg: '',
+      route: 'game',
+    });
+  }
 
-    if (userAnswer === correctAnswer) {
-      this.setState({ numCorrect: numCorrect + 1, wrongAnswerMsg: `Correct!${isGameOver ? ` Your score is ${numCorrect + 1} out of ${Object.keys(spanishVerbs).length}` : ''}` });
+  handleSubmit(e) {
+    const { input, currentLanguage, currentVerb, pronoun, numCorrect } = this.state;
+    const conjugations = this.conjugations(currentLanguage);
+    const verbs = this.verbs(currentLanguage);
+    const correctAnswer = conjugations[currentVerb][pronoun];
+
+    this.setState(prevState => ({ numAnswered: prevState.numAnswered + 1 }));
+
+    const isGameOver = this.state.numAnswered === NUM_TOTAL;
+
+    if (input === correctAnswer) {
+      this.setState({ numCorrect: numCorrect + 1, wrongAnswerMsg: `Correct!${isGameOver ? ` Your score is ${numCorrect + 1} out of ${NUM_TOTAL}` : ''}` });
     } else {
-      this.setState({ wrongAnswerMsg: `Sorry, the correct conjugation is "${correctAnswer}".${isGameOver ? ` Your score is ${numCorrect} out of ${Object.keys(spanishVerbs).length}` : ''}`})
+      this.setState({ wrongAnswerMsg: `Sorry, the correct conjugation is "${correctAnswer}".${isGameOver ? ` Your score is ${numCorrect} out of ${NUM_TOTAL}` : ''}`})
     }
-    this.setState({ verbList, input: '', isStarted: !isGameOver, pronoun: this.randomPronoun() });
+    this.setState({ input: '', pronoun: this.randomPronoun(), currentVerb: this.sample(verbs) });
+    if (isGameOver) { this.setState({ route: 'home' }); }
   }
 
   render() {
-    const { numCorrect, input, pronoun, verbList, wrongAnswerMsg, isStarted } = this.state;
+    const { route, input, pronoun, currentVerb, currentLanguage, numCorrect, numAnswered, wrongAnswerMsg } = this.state;
     return (
       <div className="App">
         <h2 className="header">{"Conjugator"}</h2>
-        {!isStarted ? (
-          <div>
-            {wrongAnswerMsg && (
-              <div className="error">
-                {wrongAnswerMsg}
+        {
+          route === 'home' && (
+            <StartScreen chooseLanguage={this.handleNext} wrongAnswerMsg={wrongAnswerMsg} />
+          )
+        }
+        {
+          route === 'study' && (
+            <StudyScreen
+              language={currentLanguage}
+              conjugations={this.conjugations(currentLanguage)}
+              pronouns={this.pronouns(currentLanguage)}
+              onNext={this.startGame}
+            />
+          )
+        }
+        {
+          route === 'game' && (
+            <div>
+              <div>{numAnswered} / {NUM_TOTAL}</div>
+              <div className="answer">
+                Conjugate the verb:
+                <br />
+                <h3>{`${currentVerb.toUpperCase()} (${this.conjugations(currentLanguage)[currentVerb].meaning})`}</h3>
+                <br />
+                {pronoun}
+                <input
+                  type="text"
+                  className="input"
+                  value={input}
+                  onKeyDown={this.handleKeyPress}
+                  onChange={this.handleChange}
+                />
               </div>
-            )}
-            <br />
-            <div onClick={this.startGame} className="button">
-              {"Start"}
-            </div>
-          </div>
-        ) : (
-          <div>
-            <div>{numCorrect} / {NUM_TOTAL}</div>
-            <div className="answer">
-              {pronoun}
-              <input
-                type="text"
-                className="input"
-                value={input}
-                onKeyDown={this.handleKeyPress}
-                onChange={this.handleChange}
-              />
-              {verbList && verbList[0]}
-            </div>
-            {wrongAnswerMsg && (
-              <div className="error">
-                {wrongAnswerMsg}
+              {wrongAnswerMsg && (
+                <div className="error">
+                  {wrongAnswerMsg}
+                </div>
+              )}
+              <div onClick={this.handleSubmit} className="button">
+                {"Submit"}
               </div>
-            )}
-            <div onClick={this.handleSubmit} className="button">
-              {"Submit"}
+              <br />
+              <br />
+              <br />
+              {"Your score: "}
+              <b>{numCorrect}</b>
             </div>
-          </div>
-        )}
+          )
+        }
       </div>
     );
   }
--- src/verbs.js
+++ src/verbs.js
@@ -1,58 +1,117 @@
-export const spanishVerbs =
-  {
-    'comer': {
-      'yo': 'como',
-      'tú': 'comes',
-      'usted': 'come',
-      'nosotros': 'comemos',
-      'ustedes': 'comen',
-      'él': 'come',
-      'ella': 'come',
-      'ellos': 'comen'
-    },
-    'hablar': {
-      'yo': 'hablo',
-      'tú': 'hablas',
-      'usted': 'habla',
-      'nosotros': 'hablamos',
-      'ustedes': 'hablan',
-      'él': 'habla',
-      'ella': 'habla',
-      'ellos': 'hablan'
-    },
-    'ir': {
-      'yo': 'voy',
-      'tú': 'vas',
-      'usted': 'va',
-      'nosotros': 'vamos',
-      'ustedes': 'van',
-      'él': 'va',
-      'ella': 'va',
-      'ellos': 'van'
-    },
-    'dar': {
-      'yo': 'doy',
-      'tú': 'das',
-      'usted': 'da',
-      'nosotros': 'damos',
-      'ustedes': 'dan',
-      'él': 'da',
-      'ella': 'da',
-      'ellos': 'dan'
-    },
-    'gustar': {
-      'yo': 'gusto',
-      'tú': 'gustas',
-      'usted': 'gusta',
-      'nosotros': 'gustamos',
-      'ustedes': 'gustan',
-      'él': 'gusta',
-      'ella': 'gusta',
-      'ellos': 'gustan'
-    }
-  };
+export const spanishPronouns = ['yo', 'nosotros', 'tú', 'ustedes', 'usted', 'ellos', 'él / ella'];
 
-export const spanishPronouns = ['yo', 'tú', 'usted', 'nosotros', 'ustedes', 'él', 'ella', 'ellos'];
+export const spanishVerbs = {
+  'comer': {
+    'meaning': 'to eat',
+    'yo': 'como',
+    'tú': 'comes',
+    'usted': 'come',
+    'nosotros': 'comemos',
+    'ustedes': 'comen',
+    'él / ella': 'come',
+    'ellos': 'comen'
+  },
+  'hablar': {
+    'meaning': 'to speak',
+    'yo': 'hablo',
+    'tú': 'hablas',
+    'usted': 'habla',
+    'nosotros': 'hablamos',
+    'ustedes': 'hablan',
+    'él / ella': 'habla',
+    'ellos': 'hablan'
+  },
+  'ir': {
+    'meaning': 'to go',
+    'yo': 'voy',
+    'tú': 'vas',
+    'usted': 'va',
+    'nosotros': 'vamos',
+    'ustedes': 'van',
+    'él / ella': 'va',
+    'ellos': 'van'
+  },
+  'dar': {
+    'meaning': 'to give',
+    'yo': 'doy',
+    'tú': 'das',
+    'usted': 'da',
+    'nosotros': 'damos',
+    'ustedes': 'dan',
+    'él / ella': 'da',
+    'ellos': 'dan'
+  },
+  'gustar': {
+    'meaning': 'to be pleasing (to smb)',
+    'yo': 'gusto',
+    'tú': 'gustas',
+    'usted': 'gusta',
+    'nosotros': 'gustamos',
+    'ustedes': 'gustan',
+    'él / ella': 'gusta',
+    'ellos': 'gustan'
+  },
+  'ser': {
+    'meaning': 'to be',
+    'yo': 'soy',
+    'tú': 'eres',
+    'usted': 'es',
+    'nosotros': 'somos',
+    'ustedes': 'son',
+    'él / ella': 'es',
+    'ellos': 'son'
+  },
+  'haber': {
+    'meaning': 'to have (auxiliary)',
+    'yo': 'he',
+    'tú': 'has',
+    'usted': 'ha',
+    'nosotros': 'hemos',
+    'ustedes': 'han',
+    'él / ella': 'ha',
+    'ellos': 'han'
+  },
+  'tener': {
+    'meaning': 'to have (object)',
+    'yo': 'tengo',
+    'tú': 'tienes',
+    'usted': 'tiene',
+    'nosotros': 'tenemos',
+    'ustedes': 'tienen',
+    'él / ella': 'tienen',
+    'ellos': 'tienen'
+  },
+  'hacer': {
+    'meaning': 'to do/make',
+    'yo': 'hago',
+    'tú': 'haces',
+    'usted': 'hace',
+    'nosotros': 'hacemos',
+    'ustedes': 'hacen',
+    'él / ella': 'hace',
+    'ellos': 'hacen'
+  },
+  'poder': {
+    'meaning': 'to be able',
+    'yo': 'puedo',
+    'tú': 'puedes',
+    'usted': 'puede',
+    'nosotros': 'podemos',
+    'ustedes': 'pueden',
+    'él / ella': 'puede',
+    'ellos': 'pueden'
+  },
+  'decir': {
+    'meaning': 'to say',
+    'yo': 'digo',
+    'tú': 'dices',
+    'usted': 'dice',
+    'nosotros': 'decimos',
+    'ustedes': 'dicen',
+    'él / ella': 'dice',
+    'ellos': 'dicen'
+  }
+};
 
 export const russianVerbs = [
   {
@@ -79,25 +138,87 @@ export const russianVerbs = [
   }
 ];
 
-export const germanVerbs = [
-  {
-    'essen': {
-      'ich': 'esse',
-      'du': 'isst',
-      'Sie': 'essen',
-      'wir': 'essen',
-      'er': 'isst',
-      'sie': 'isst',
-    }
+export const germanPronouns = ['ich', 'wir', 'du', 'ihr', 'Sie', 'sie (they)', 'er / sie'];
+
+export const germanVerbs = {
+  'arbeiten': {
+    'meaning': 'to work',
+    'ich': 'arbeite',
+    'du': 'arbeitest',
+    'Sie': 'arbeiten',
+    'wir': 'arbeiten',
+    'ihr': 'arbeitet',
+    'er / sie': 'arbeitet',
+    'sie (they)': 'arbeiten',
   },
-  {
-    'sprechen': {
-      'ich': 'spreche',
-      'du': 'sprichst',
-      'Sie': 'sprechen',
-      'wir': 'sprechen',
-      'er': 'spricht',
-      'sie': 'spricht',
-    }
-  }
-];
+  'reisen': {
+    'meaning': 'to travel',
+    'ich': 'reise',
+    'du': 'reist',
+    'Sie': 'reisen',
+    'wir': 'reisen',
+    'er / sie': 'reist',
+    'ihr': 'reist',
+    'sie (they)': 'reisen',
+  },
+  'gehen': {
+    'meaning': 'to go',
+    'ich': 'gehe',
+    'du': 'gehst',
+    'Sie': 'gehen',
+    'wir': 'gehen',
+    'er / sie': 'geht',
+    'ihr': 'geht',
+    'sie (they)': 'gehen',
+  },
+  'heissen': {
+    'meaning': 'to be named',
+    'ich': 'heisse',
+    'du': 'heisst',
+    'Sie': 'heissen',
+    'wir': 'heissen',
+    'er / sie': 'heisst',
+    'ihr': 'heisst',
+    'sie (they)': 'heissen',
+  },
+  'spielen': {
+    'meaning': 'to play',
+    'ich': 'spiele',
+    'du': 'spielst',
+    'Sie': 'spielen',
+    'wir': 'spielen',
+    'er / sie': 'spielt',
+    'ihr': 'spielt',
+    'sie (they)': 'spielen',
+  },
+  'suchen': {
+    'meaning': 'to search',
+    'ich': 'suche',
+    'du': 'suchst',
+    'Sie': 'suchen',
+    'wir': 'suchen',
+    'er / sie': 'sucht',
+    'ihr': 'sucht',
+    'sie (they)': 'suchen',
+  },
+  'lernen': {
+    'meaning': 'to learn',
+    'ich': 'lerne',
+    'du': 'lernst',
+    'Sie': 'lernen',
+    'wir': 'lernen',
+    'er / sie': 'lernt',
+    'ihr': 'lernt',
+    'sie (they)': 'lernen',
+  },
+  'tanzen': {
+    'meaning': 'to dance',
+    'ich': 'tanze',
+    'du': 'tanzt',
+    'Sie': 'tanzen',
+    'wir': 'tanzen',
+    'er / sie': 'tanzt',
+    'ihr': 'tanzt',
+    'sie (they)': 'tanzen',
+  },
+};
